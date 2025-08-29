7b2cea281c8bf3234bb016442a1cb4e50dd697c1
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2025-08-29 12:38:41 +0530

    Time: 136 ms (19.41%) | Memory: 43.6 MB (17.28%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+const vector<int> dr = {1, -1, 0 ,0};
+const vector<int> dc = {0, -0, -1 ,1};
+
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> memo(n, vector<int>(m, INT_MAX));
+        using T = pair<int,pair<int,int>>;
+        queue<T> pq;
+        memo[0][0] = 0;
+        pq.push({memo[0][0], {0,0}});
+        int ans = INT_MAX;
+
+        while(!pq.empty()) {
+            auto pair = pq.front();
+            pq.pop();
+            int minHeight = pair.first;
+            auto [i, j] = pair.second;
+            if(i == n-1 && j == m-1) {
+                ans = min(ans, memo[i][j]);
+            }
+            for(int k = 0; k < 4; k++) {
+                int nr = i + dr[k];
+                int nc = j + dc[k];
+                int new_min = INT_MAX;
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                    int diff = abs(grid[nr][nc] - grid[i][j]);
+                    int newEffort = max(memo[i][j], diff);
+if (newEffort < memo[nr][nc]) {
+    memo[nr][nc] = newEffort;
+    pq.push({newEffort, {nr, nc}});
+}
+                }
+            }
+        }
+        return ans;
+    }
+};
