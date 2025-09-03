c153b3bb1e92e37d7bd7fff1f371a7d5757c682a
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2025-09-04 01:07:23 +0530

    Time: 119 ms (76.56%) | Memory: 44.1 MB (19.87%) - LeetSync

--- 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-        sort(points.begin(), points.end(), [] (auto& a, auto& b) {
-            if(a[0] == b[0]) return a[1] > b[1];
-            return a[0] < b[0];
-        });
+        sort(points.begin(), points.end(), [] (auto& a, auto& b) {if(a[0] == b[0]) return a[1] > b[1];return a[0] < b[0];});
         vector<int> vis(1001, -1);
-        int count = 0;
-        int n = points.size();
-        for(int i = 0; i < n-1; i++) {
-            int prev = INT_MAX;
-            for(int j = i+1; j < n; j++) {
+        int count = 0, n = points.size();
+        for(int i = 0; i < n-1; i++) {int prev = INT_MAX;for(int j = i+1; j < n; j++) {
                bool flag = true;
-               int c1 = points[i][1];
-               int c2 = points[j][1];
+               int c1 = points[i][1], c2 = points[j][1];
                if(c2 > c1) continue;
-               else {
-                if(prev == INT_MAX || prev < c2) {
-                    // cout<<c1<<" "<<c2<<" "<<prev<<endl;
-                    count++;
-                    prev = c2;
-                }
-               }
-            }
+                if(prev == INT_MAX || prev < c2) {count++;prev = c2;}
+        }
         }
         return count;
     }
