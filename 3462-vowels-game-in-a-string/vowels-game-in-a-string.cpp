a3181672d1671344df9b2a467631502efb581c17
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2025-09-12 10:41:38 +0530

    Time: 4 ms (78.54%) | Memory: 19.1 MB (25.75%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        bool flag = false;
-        for(auto it : s) if(it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u') flag = true;
-    return flag;
+        for(auto it : s) if(it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u') return true;
+    return false;
     }
 };
