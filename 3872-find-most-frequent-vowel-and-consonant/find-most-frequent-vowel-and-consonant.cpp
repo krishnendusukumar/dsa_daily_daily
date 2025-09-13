3df756f92ebb6e9d2b6d010ba5d927a6da9a586f
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2025-09-13 10:14:26 +0530

    Time: 3 ms (37.49%) | Memory: 9.8 MB (44.54%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int maxFreqSum(string s) {
         unordered_map<int,int>m;
-        for(int i = 0; i < s.size(); i++) {
-            m[s[i] - 'a']++;
-        }
         int ans = 0, count = 0;
-        for(auto it : m) {
-            auto first = it.first + 'a';
-            if(first == 'a' || first == 'e' || first == 'i' || first == 'o' || first == 'u') ans = max(ans, it.second);
-            else count = max(count, it.second);
+        for(int i = 0; i < s.size(); i++) {
+            auto first = s[i];
+            auto second = ++m[s[i] - 'a'];
+            if(first == 'a' || first == 'e' || first == 'i' || first == 'o' || first == 'u') ans = max(ans, second);
+            else count = max(count, second);
         }
         return ans + count;
     }
