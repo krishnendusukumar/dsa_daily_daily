a7ced2903b5d193ce36e60d3e4aecc200c642a6a
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2025-09-13 10:19:26 +0530

    Time: 4 ms (19.67%) | Memory: 9.9 MB (44.54%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -4,10 +4,8 @@ public:
         map<int,int>m;
         int ans = 0, count = 0;
         for(int i = 0; i < s.size(); i++) {
-            auto first = s[i];
-            auto second = ++m[s[i] - 'a'];
-            if(first == 'a' || first == 'e' || first == 'i' || first == 'o' || first == 'u') ans = max(ans, second);
-            else count = max(count, second);
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') ans = max(ans, ++m[s[i] - 'a']);
+            else count = max(count, ++m[s[i] - 'a']);
         }
         return ans + count;
     }
