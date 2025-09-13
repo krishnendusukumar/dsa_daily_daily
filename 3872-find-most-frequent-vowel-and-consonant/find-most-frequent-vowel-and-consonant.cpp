81fdc48c6adc9df653a7247a8d69af57789fd82f
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2025-09-13 10:10:25 +0530

    Time: 2 ms (45.68%) | Memory: 9.8 MB (57.27%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        map<int,int>m;
+        for(int i = 0; i < s.size(); i++) {
+            m[s[i] - 'a']++;
+        }
+        int ans = 0, count = 0;
+        for(auto it : m) {
+            auto first = it.first + 'a';
+            if(first == 'a' || first == 'e' || first == 'i' || first == 'o' || first == 'u') ans = max(ans, it.second);
+            else count = max(count, it.second);
+        }
+        return ans + count;
+    }
+};
